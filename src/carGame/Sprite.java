ff29eb8633e7ade66bfe65da51db79de6bc4f6bf
Burhan <burhan.kvk58@gmail.com>
2023-01-12 11:22:00 +0300

    Super Class Oluşturuldu

--- src/carGame/Sprite.java
+++ src/carGame/Sprite.java
@@ -1,4 +1,67 @@
 package carGame;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Sprite {
+
+    protected int x;
+    protected int y;
+    protected int width;
+    protected int height;
+    protected boolean visible;
+    protected Image image;
+
+    public Sprite(int x, int y) {
+        this.x = x;
+        this.y = y;
+        visible = true;
+    }
+
+
+    protected void loadImage(String imageName) {
+        ImageIcon imageIcon = new ImageIcon(imageName);
+        image = imageIcon.getImage();
+    }
+
+    protected void getImageDimensions() {
+        width = image.getWidth(null);
+        height = image.getHeight(null);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+
+
+
+
 }
